4999efa731113d78541beca00469386a291934d5
G Adarsh <gadarsh@Adarshs-Macbook.local>
2022-11-16 23:58:48 +0530

    Initial Commit

---
+++ src/views/DemoPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="game container">
+      Demo Page
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/main';
+.game {
+  @include font-light(16px, 1)
+}
+</style>
+
+<script src="./js/demopage.js"></script>
